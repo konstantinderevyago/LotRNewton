217e7771cda4bc469e3f7a693fdeff4d940a1212
Konstantin <jetfirer@gmail.com>
2017-02-06 19:04:55 +0300

    View pager

---
+++ app/src/main/java/by/jetfire/lotrnewton/MainPagerAdapter.java
@@ -0,0 +1,32 @@
+package by.jetfire.lotrnewton;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Konstantin on 06.02.2017.
+ */
+
+public class MainPagerAdapter extends FragmentStatePagerAdapter {
+
+    public MainPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new TaskFragment();
+            case 1:
+                return new ConditionsFragment();
+        }
+        return null;
+    }
+}
